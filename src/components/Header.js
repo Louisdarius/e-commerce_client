bd0b92ff133ff30b9d195e43d2bb92f8095c4c80
Louis_darius <louisdarius96@yahoo.fr>
2022-03-09 14:20:21 +0000

    set up structure

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+const Header = () => {
+  return (
+    <div>
+      <p>Header</p>
+      <Button variant='primary'>Click me</Button>{' '}
+    </div>
+  );
+};
+
+export default Header;
